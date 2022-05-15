fd3cb61a2b1c20037e693a2274dc7f382a80fb8b
Adam Lloyd <lloyd.adam99@gmail.com>
2022-05-15 11:41:53 +0200

    Filmen skapas med rätt komponenter

--- app/src/Movie.js
+++ app/src/Movie.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './Movie.css';
 
-export default function Movie() {
+export default function Movie(props) {
+
+    // Lägger till en stjärna beroende på rating
+    const stars = [];
+    for (var i = 0; i < props.item.rating; i++) {
+        stars.push(<img src="images/star.png" alt="star" />)
+    }
+
   return (
-    <div>Movie</div>
+    // Skapar en film med rätt komponenter
+    <li className='list-group-item' data-grade={props.item.rating} data-title={props.item.title}>
+        { props.item.title }
+        <img src="images/delete.png" alt="Delete movie" className="delete-movie-icon" onClick={() => {props.deleteMovie(props.item.id )}} />
+        { stars }
+    </li>
   )
 }
--- app/src/Movies.js
+++ app/src/Movies.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import Movie from './Movie'
+import SortByRating from './SortByRating'
+import SortByAlpha from './SortByAlpha'
 
-export default function Movies() {
+export default function Movies(props) {
+
+  // Returnerar listan med filmer samt knappar för att kunna sortera
   return (
-    <div>Movies</div>
+    <div>
+        <h2 className="mt-3">Inlagda filmer</h2>
+
+        <ul className='list-group'>
+            {props.movieList.map(movie => <Movie key={movie.id} item={movie} deleteMovie={props.deleteMovie}  />)}
+        </ul>
+
+        <SortByAlpha movies={props.movieList} setMovies={props.setMovies} />
+        <SortByRating movies={props.movieList} setMovies={props.setMovies} />
+     </div>
   )
 }
