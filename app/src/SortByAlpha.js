e9f794c29a7fa6eb2b33fda78cb80772bc297b73
Adam Lloyd <lloyd.adam99@gmail.com>
2022-05-15 11:42:46 +0200

    Sortera baserat på titel

--- app/src/SortByAlpha.js
+++ app/src/SortByAlpha.js
@@ -1,7 +1,17 @@
 import React from 'react'
 
-export default function SortByAlpha() {
+export default function sortByAlpha(props) {
+
+  function sortMovies() {
+    /**
+    * Sorterar listan genom alfabetisk ordning
+    */
+    let sortedItems = props.movies.sort((a,b) => a.title.localeCompare(b.title))
+    props.setMovies(Array.from(sortedItems))
+  }
+
+  // Returnerar knappen för sortering i alfabetisk ordning
   return (
-    <div>SortByAlpha</div>
+    <button className="btn btn-primary mt-2 ms-1" onClick={sortMovies}>Alfabetisk ordning</button>
   )
 }
