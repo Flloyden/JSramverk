d8431749ddf0036ac1b34c4ecfc4d97b2c964818
Adam Lloyd <lloyd.adam99@gmail.com>
2022-05-15 11:42:37 +0200

    Sortera baserat på betyg

--- app/src/SortByRating.js
+++ app/src/SortByRating.js
@@ -1,7 +1,17 @@
 import React from 'react'
 
-export default function SortByRating() {
+export default function SortByGrade(props) {
+
+    function sortMovies() {
+      /**
+      * Sorterar listan genom betyg på filmen
+      */
+      let sortedMovieList = props.movies.sort((a, b) => b.rating - a.rating);
+      props.setMovies(Array.from(sortedMovieList))
+    }
+
+  // Returnerar knappen för sortering i alfabetisk ordning
   return (
-    <div>SortByRating</div>
+    <button className="btn btn-primary mt-2 ms-1" onClick={sortMovies}>Betygsordning</button>
   )
 }
